86f3d2dbe26f5be48b96cb3beb359e449cc5608c
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-05-05 09:53:29 -0500

    feat: implement JPA auditing and OpenAPI
    - Enables JPA auditing for automatic tracking of creation and modification timestamps on entities.
    - Introduces AuditableAbstractAggregateRoot and AuditableModel for consistent auditing across aggregates and entities.
    - Configures a custom naming strategy to use snake case with pluralized table names.
    - Adds OpenAPI configuration for API documentation.

--- src/main/java/com/campusmov/platform/matchingroutingservice/MatchingRoutingServiceApplication.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/MatchingRoutingServiceApplication.java
@@ -2,8 +2,10 @@ package com.campusmov.platform.matchingroutingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MatchingRoutingServiceApplication {
 
     public static void main(String[] args) {
