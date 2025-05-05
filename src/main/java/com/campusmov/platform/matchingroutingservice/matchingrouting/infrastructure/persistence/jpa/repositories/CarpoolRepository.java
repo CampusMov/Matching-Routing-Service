301f5c26c1bc7eb732b45560737f52cd3cd6fa44
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-05-05 15:20:30 -0500

    feat(carpool): prevent driver from creating multiple carpools
    Adds a check to prevent a driver from creating a new carpool if they are already associated with an active carpool (CREATED or IN_PROGRESS).
    
    This change ensures that drivers can only participate in one carpool at a time, preventing potential conflicts and improving the overall system integrity.

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/infrastructure/persistence/jpa/repositories/CarpoolRepository.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/infrastructure/persistence/jpa/repositories/CarpoolRepository.java
@@ -1,9 +1,14 @@
 package com.campusmov.platform.matchingroutingservice.matchingrouting.infrastructure.persistence.jpa.repositories;
 
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.aggregates.Carpool;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.valueobjects.ECarpoolStatus;
+import com.campusmov.platform.matchingroutingservice.shared.domain.model.valueobjects.DriverId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarpoolRepository extends JpaRepository<Carpool, String> {
+    Boolean existsByDriverIdAndStatusIn(DriverId driverId, List<ECarpoolStatus> statuses);
 }
