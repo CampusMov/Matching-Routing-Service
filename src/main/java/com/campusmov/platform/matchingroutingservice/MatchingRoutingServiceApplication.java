32c1b4bb0b6df021edc4e348e3adb783db6bbb38
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-05-05 09:22:59 -0500

    chore: init commit

---
+++ src/main/java/com/campusmov/platform/matchingroutingservice/MatchingRoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campusmov.platform.matchingroutingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingRoutingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingRoutingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campusmov/platform/matchingroutingservice/MatchingRoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusmov.platform.matchingroutingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingRoutingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
