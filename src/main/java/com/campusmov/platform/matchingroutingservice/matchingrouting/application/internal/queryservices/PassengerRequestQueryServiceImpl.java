b6a39903b4aad358897f04b93edf2102221990bd
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-05-07 16:48:18 -0500

    feat(passenger-request): create query to get all passenger request by carpool id
    Include in service and use repository in service implementation.

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/application/internal/queryservices/PassengerRequestQueryServiceImpl.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/application/internal/queryservices/PassengerRequestQueryServiceImpl.java
@@ -1,12 +1,16 @@
 package com.campusmov.platform.matchingroutingservice.matchingrouting.application.internal.queryservices;
 
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.aggregates.PassengerRequest;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.queries.GetAllPassengerRequestsByCarpoolIdAndStatusIsPendingQuery;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.queries.GetPassengerRequestByIdQuery;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.valueobjects.EPassengerRequestStatus;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.services.PassengerRequestQueryService;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.infrastructure.persistence.jpa.repositories.PassengerRequestRepository;
+import com.campusmov.platform.matchingroutingservice.shared.domain.model.valueobjects.CarpoolId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -17,5 +21,10 @@ public class PassengerRequestQueryServiceImpl implements PassengerRequestQuerySe
     @Override
     public Optional<PassengerRequest> handle(GetPassengerRequestByIdQuery query) {
         return passengerRequestRepository.findById(query.passengerRequestId());
+    }
+
+    @Override
+    public Collection<PassengerRequest> handle(GetAllPassengerRequestsByCarpoolIdAndStatusIsPendingQuery query) {
+        return passengerRequestRepository.findAllByCarpoolIdAndStatusIs(new CarpoolId(query.carpoolId()), EPassengerRequestStatus.PENDING);
     }
 }
