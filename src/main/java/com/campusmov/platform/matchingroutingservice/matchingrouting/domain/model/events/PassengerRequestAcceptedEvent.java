7988754d81751fb4f6478f3aec110fbbf18f1be9
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-05-08 08:35:46 -0500

    feat(events): add sealed interface for PassengerRequestEvent and implementation
    Introduced a sealed interface `PassengerRequestEvent` and implemented `PassengerRequestAcceptedEvent` with relevant fields and Builder pattern.

---
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/events/PassengerRequestAcceptedEvent.java
@@ -0,0 +1,16 @@
+package com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.events;
+
+import lombok.Builder;
+
+@Builder
+public record PassengerRequestAcceptedEvent(
+        String passengerRequestId,
+        String passengerId,
+        String pickupLocationName,
+        String pickupLocationAddress,
+        Double pickupLocationLongitude,
+        Double pickupLocationLatitude,
+        Integer requestedSeats,
+        String status
+) implements PassengerRequestEvent {
+}
