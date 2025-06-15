6a9a505590986ff0c35a59dc5bd58dfaafe10f9b
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-15 18:10:35 -0500

    feat(carpool): add startedClassTime and endedClassTime fields

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/aggregates/Carpool.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/aggregates/Carpool.java
@@ -14,6 +14,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -59,6 +60,12 @@ public class Carpool extends AuditableAbstractAggregateRoot<Carpool> {
             @AttributeOverride(name = "latitude",  column = @Column(name = "dest_latitude"))
     })
     private Location destination;
+
+    @NotNull
+    private LocalDateTime startedClassTime;
+
+    @NotNull
+    private LocalDateTime endedClassTime;
 
     @NotNull
     private Boolean isVisible;
