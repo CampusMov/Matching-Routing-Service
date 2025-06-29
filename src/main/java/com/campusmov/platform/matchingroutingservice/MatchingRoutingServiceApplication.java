50a5d7407659b51fcd8acc1a9a7b5af41925efa0
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-29 16:58:20 -0500

    feat: configures Neo4j integration
    - Creates a Neo4j configuration class to manage database connection and transaction management.
    - Enables Neo4j repositories and auditing.
    - Configures beans for Driver, Neo4jClient, Neo4jTemplate, and Neo4jMappingContext.

--- src/main/java/com/campusmov/platform/matchingroutingservice/MatchingRoutingServiceApplication.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/MatchingRoutingServiceApplication.java
@@ -3,9 +3,15 @@ package com.campusmov.platform.matchingroutingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableNeo4jAuditing
+@EnableJpaRepositories
+@EnableNeo4jRepositories
 public class MatchingRoutingServiceApplication {
 
     public static void main(String[] args) {
