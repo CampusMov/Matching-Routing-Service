01d638abf9b382cea22fa4b9f77cfc185be1fd4d
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-03 10:54:37 -0500

    feat(event): emits event on request accepted

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/events/PassengerRequestAcceptedEvent.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/events/PassengerRequestAcceptedEvent.java
@@ -1,17 +1,41 @@
 package com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.events;
 
-import lombok.Builder;
+import com.campusmov.platform.matchingroutingservice.shared.domain.model.events.DomainEvent;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
 
-@Builder
-public record PassengerRequestAcceptedEvent(
-        String passengerRequestId,
-        String carpoolId,
-        String passengerId,
-        String pickupLocationName,
-        String pickupLocationAddress,
-        Double pickupLocationLongitude,
-        Double pickupLocationLatitude,
-        Integer requestedSeats,
-        String status
-) implements PassengerRequestEvent {
+@Getter
+public final class PassengerRequestAcceptedEvent extends ApplicationEvent implements DomainEvent {
+    private final String passengerRequestId;
+    private final String carpoolId;
+    private final String passengerId;
+    private final String pickupLocationName;
+    private final String pickupLocationAddress;
+    private final Double pickupLocationLongitude;
+    private final Double pickupLocationLatitude;
+    private final Integer requestedSeats;
+    private final String status;
+
+    public PassengerRequestAcceptedEvent(
+            String passengerRequestId,
+            String carpoolId,
+            String passengerId,
+            String pickupLocationName,
+            String pickupLocationAddress,
+            Double pickupLocationLongitude,
+            Double pickupLocationLatitude,
+            Integer requestedSeats,
+            String status
+    ) {
+        super("PassengerRequestService");
+        this.passengerRequestId = passengerRequestId;
+        this.carpoolId = carpoolId;
+        this.passengerId = passengerId;
+        this.pickupLocationName = pickupLocationName;
+        this.pickupLocationAddress = pickupLocationAddress;
+        this.pickupLocationLongitude = pickupLocationLongitude;
+        this.pickupLocationLatitude = pickupLocationLatitude;
+        this.requestedSeats = requestedSeats;
+        this.status = status;
+    }
 }
--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/events/PassengerRequestEvent.java
+++
@@ -1,7 +0,0 @@
-package com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.events;
-
-import com.campusmov.platform.matchingroutingservice.shared.domain.model.events.DomainEvent;
-
-public sealed interface PassengerRequestEvent extends DomainEvent permits
-        PassengerRequestAcceptedEvent {
-}
