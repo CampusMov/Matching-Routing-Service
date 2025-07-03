1401a9a8a98b43c82abbd9692b760cf3c1dff014
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-03 16:01:08 -0500

    feat(websocket): Implements carpool status updates
    Adds a WebSocket publisher service to broadcast carpool
    status updates (started/cancelled) to clients.

---
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/application/internal/outboundservices/CarpoolWebSocketPublisherService.java
@@ -0,0 +1,22 @@
+package com.campusmov.platform.matchingroutingservice.matchingrouting.application.internal.outboundservices;
+
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.payloads.CarpoolStartedPayload;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.payloads.CarpoolCancelledPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CarpoolWebSocketPublisherService {
+    private final SimpMessagingTemplate messagingTemplate;
+    public void handleStartedCarpool(CarpoolStartedPayload carpoolStartedPayload) {
+        String destination = "/topic/carpool/" + carpoolStartedPayload.id() + "/status";
+        messagingTemplate.convertAndSend(destination, carpoolStartedPayload);
+    }
+
+    public void handleCancelledCarpool(CarpoolCancelledPayload carpoolCancelledPayload) {
+        String destination = "/topic/carpool/" + carpoolCancelledPayload.id() + "/status";
+        messagingTemplate.convertAndSend(destination, carpoolCancelledPayload);
+    }
+}
