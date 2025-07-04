862bc7a1c7dbe92c9bd48387fb19c7545568aedc
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-03 21:32:39 -0500

    refactor: change carpool id for route id

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/commands/CreateWayPointCommand.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/model/commands/CreateWayPointCommand.java
@@ -1,13 +1,13 @@
 package com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands;
 
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.valueobjects.Location;
+import com.campusmov.platform.matchingroutingservice.shared.domain.model.valueobjects.CarpoolId;
 import com.campusmov.platform.matchingroutingservice.shared.domain.model.valueobjects.PassengerId;
-import com.campusmov.platform.matchingroutingservice.shared.domain.model.valueobjects.RouteId;
 import lombok.Builder;
 
 @Builder
 public record CreateWayPointCommand(
-        RouteId routeId,
+        CarpoolId carpoolId,
         PassengerId passengerId,
         Location location
 ) {
