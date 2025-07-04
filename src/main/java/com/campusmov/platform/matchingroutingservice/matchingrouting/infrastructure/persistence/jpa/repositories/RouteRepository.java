7d3a61909cf31f04b678f4004c9843017d36e004
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-03 21:33:58 -0500

    feat: add route repository
    Add find by carpool id

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/infrastructure/persistence/jpa/repositories/RouteRepository.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/infrastructure/persistence/jpa/repositories/RouteRepository.java
@@ -1,9 +1,13 @@
 package com.campusmov.platform.matchingroutingservice.matchingrouting.infrastructure.persistence.jpa.repositories;
 
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.aggregates.Route;
+import com.campusmov.platform.matchingroutingservice.shared.domain.model.valueobjects.CarpoolId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RouteRepository extends JpaRepository<Route, String> {
+    Optional<Route> findByCarpoolId(CarpoolId carpoolId);
 }
