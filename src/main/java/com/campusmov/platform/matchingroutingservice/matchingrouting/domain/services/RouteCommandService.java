6b7d943f89ad6f2197eba34c5fa1a3f26e11b0f5
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-10 02:17:44 -0500

    feat(route): implements update current location
    Implements the ability to update the current location of a route.
    This includes persisting the updated location and publishing
    a websocket event to notify clients of the change.

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/services/RouteCommandService.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/domain/services/RouteCommandService.java
@@ -3,6 +3,7 @@ package com.campusmov.platform.matchingroutingservice.matchingrouting.domain.ser
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.aggregates.Route;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.CreateRouteCommand;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.CreateWayPointCommand;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.UpdateCurrentLocationRouteCommand;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.entities.WayPoint;
 
 import java.util.Optional;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface RouteCommandService {
     Optional<Route> handle(CreateRouteCommand command);
     Optional<WayPoint> handle(CreateWayPointCommand command);
+    Optional<Route> handle(UpdateCurrentLocationRouteCommand command);
 }
