2e633346f565a386b2323fd189b4003f9b136eae
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-10 09:34:46 -0500

    feat: add CarpoolCompletedPayload and its transform

---
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/application/internal/outboundservices/transform/CarpoolCompletedPayloadFromEntityAssembler.java
@@ -0,0 +1,31 @@
+package com.campusmov.platform.matchingroutingservice.matchingrouting.application.internal.outboundservices.transform;
+
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.aggregates.Carpool;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.payloads.CarpoolCompletedPayload;
+
+public class CarpoolCompletedPayloadFromEntityAssembler {
+    public static CarpoolCompletedPayload fromEntity(Carpool entity) {
+        return CarpoolCompletedPayload.builder().
+                id(entity.getId()).
+                driverId(entity.getDriverId().driverId()).
+                vehicleId(entity.getVehicleId().vehicleId()).
+                status(entity.getStatus().toString()).
+                availableSeats(entity.getAvailableSeats()).
+                maxPassengers(entity.getMaxPassengers()).
+                scheduleId(entity.getScheduleId().scheduleId()).
+                radius(entity.getRadius()).
+                originName(entity.getOrigin().getName()).
+                originAddress(entity.getOrigin().getAddress()).
+                originLatitude(entity.getOrigin().getLatitude()).
+                originLongitude(entity.getOrigin().getLongitude()).
+                destinationName(entity.getDestination().getName()).
+                destinationAddress(entity.getDestination().getAddress()).
+                destinationLatitude(entity.getDestination().getLatitude()).
+                destinationLongitude(entity.getDestination().getLongitude()).
+                startedClassTime(entity.getStartedClassTime()).
+                endedClassTime(entity.getEndedClassTime()).
+                classDay(entity.getClassDay().toString()).
+                isVisible(entity.getIsVisible()).
+                build();
+    }
+}
