e114fb4f08c71e84e4b75206869b262aa911b9bf
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-07-10 02:24:18 -0500

    feat(carpool): implement finish carpool command
    Adds functionality to mark a carpool as finished.
    
    This includes:
    - Adding a `FinishCarpoolCommand` and handler.
    - Updating the `CarpoolCommandService` interface and implementation.
    - Updating the Carpool aggregate to support the finish operation

--- src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/application/internal/commandservices/CarpoolCommandServiceImpl.java
+++ src/main/java/com/campusmov/platform/matchingroutingservice/matchingrouting/application/internal/commandservices/CarpoolCommandServiceImpl.java
@@ -5,6 +5,7 @@ import com.campusmov.platform.matchingroutingservice.matchingrouting.application
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.aggregates.Carpool;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.CreateCarpoolCommand;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.CreateLinkedPassengerCommand;
+import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.FinishCarpoolCommand;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.commands.StartCarpoolCommand;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.model.valueobjects.ECarpoolStatus;
 import com.campusmov.platform.matchingroutingservice.matchingrouting.domain.services.CarpoolCommandService;
@@ -53,6 +54,20 @@ public class CarpoolCommandServiceImpl implements CarpoolCommandService {
             throw new RuntimeException("Error saving carpool", e);
         }
         return Optional.of(carpool);
+    }
+
+    @Override
+    public Optional<Carpool> handle(FinishCarpoolCommand command) {
+        Carpool carpool = carpoolRepository
+                .findById(command.carpoolId())
+                .orElseThrow(() -> new IllegalArgumentException("Carpool with ID %s not found".formatted(command.carpoolId())));
+        carpool.finish();
+        try {
+            var finishedCarpool = carpoolRepository.save(carpool);
+            return Optional.of(finishedCarpool);
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving carpool", e);
+        }
     }
 
     @Override
